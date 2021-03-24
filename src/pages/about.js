6d9a499aa9658d89d54fb64c51c9a0bc0f6fa266
Silvia Mandala <sil.mandala@gmail.com>
2021-03-24 23:28:42 +0000

    add client list

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,50 +41,97 @@ For the past 12 years, I've worked as a Creative Design Lead with over 50 client
           <p><a href={data.pdf.publicURL} target="_blank">Download my CV</a></p>
           </div>
           </div>
+          <h1>Worked with</h1>
 
-          <div className="project-info">
-          <h1>Client List</h1>
-
+          <div className="client-list">
             <div className="padded-total">
               <ul> 
-                <h3>Sector1</h3>
-                <li>Client1</li>
-                <li>Client2</li>
-                <li>Client3</li>
+                <h3>Banks, Finance, Hedge Funds</h3>
+                <li>City Asset Management</li>
+                <li>City Equities Ltd.</li>
+                <li>Earth Capital Partners Llp.</li>
+                <li>Hengistbury Investment Partners</li>
+                <li>Jupiter Asset Mgt.</li>
+                <li>Kairos</li>
+                <li>Key Square Capital Management</li>
+                <li>Marubeni Europe Plc.</li>
+                <li>Polunin Capital Partners</li>
+                <li>Raymond James Investment Services</li>
+                <li>Sator Group</li>
+                <li>Stratton Street Capital Llp.</li>
               </ul>
               <ul> 
-                <h3>Sector2</h3>
-                <li>Client1</li>
-                <li>Client2</li>
-                <li>Client3</li>
+                <h3>Charity</h3>
+                <li>British Red Cross</li>
+                <li>Christian Aid</li>
+                <li>RNBI</li>
+                <li>YMCA</li>
+              </ul>
+              <ul> 
+                <h3>Education</h3>
+                <li>Kaplan Inc.</li>
+                <li>Shillingtons</li>
+              </ul>
+              <ul> 
+                <h3>Oil, GAS, Utilities</h3>
+                <li>Savannah Petroleum</li>
+                <li>Investors</li>
+              </ul>
+              <ul> 
+                <h3>Services</h3>
+                <li>The Royal Institute of International Affairs</li>
+                <li>World Nuclear Association</li>
+              </ul>
+              <ul> 
+                <h3>PR & Advertising</h3>
+                <li>Brands2Life</li>
+                <li>Sportquake</li>
+              </ul>
+              <ul> 
+                <h3>Property</h3>
+                <li>Mellersh and Harding</li>
+                <li>Wilks Head and Eve</li>
               </ul>
             </div>
             <div className="padded-total">
             <ul> 
-                <h3>Sector3</h3>
-                <li>Client1</li>
-                <li>Client2</li>
-                <li>Client3</li>
+                <h3>Insurance</h3>
+                <li>Allianz Insurance</li>
+                <li>Navigators</li>
+                <li>Price Forbes</li>
+                <li>Simply Business</li>
               </ul>
               <ul> 
-                <h3>Sector1</h3>
-                <li>Client1</li>
-                <li>Client2</li>
-                <li>Client3</li>
-              </ul>
-            </div>
-            <div className="padded-total">
-            <ul> 
-                <h3>Sector4</h3>
-                <li>Client1</li>
-                <li>Client2</li>
-                <li>Client3</li>
+                <h3>IT</h3>
+                <li>Base kit</li>
+                <li>Double Edge Professional Services</li>
+                <li>Ebury Partners</li>
+                <li>First Source</li>
               </ul>
               <ul> 
-                <h3>Sector1</h3>
-                <li>Client1</li>
-                <li>Client2</li>
-                <li>Client3</li>
+                <h3>Legal</h3>
+                <li>Doughty Street Chambers</li>
+                <li>Penningtons Manches Llp.</li>
+                <li>RGC Jenkins</li>
+                <li>Wilberforce Chambers</li>
+              </ul>
+              <ul> 
+                <h3>Pharmaceuticals</h3>
+                <li>Pfeizer</li>
+              </ul>
+              <ul> 
+                <h3>Transport</h3>
+                <li>Federal Express</li>
+                <li>Hitachi Rail Europe Ltd.</li>
+              </ul>
+              <ul> 
+                <h3>Manufacturing</h3>
+                <li>Hitachi Europe</li>
+                <li>YKK Europe Ltd.</li>
+              </ul>
+                <ul> 
+                <h3>Media</h3>
+                <li>Incisive Media Business</li>
               </ul>
             </div>
             </div>
