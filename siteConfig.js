a801bdec7676e150d698b82c83adcbfe0ec2bd6b
Silvia Mandala <sil.mandala@gmail.com>
2021-03-24 14:07:00 +0000

    Updates

--- siteConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'Hoda Hatami',
-  shortName: 'Hoda',
-  description: 'A portfolio',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Silvia Mandalà',
-  linkedin: 'hoda-hatami'
-}
